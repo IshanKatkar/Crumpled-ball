9f7ad179c42038e42301863705f3225a6a73ab25
IshanKatkar <73028425+IshanKatkar@users.noreply.github.com>
2020-12-19 20:02:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var dustbinobj,ball,groundobj;
+
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	dustbinobj=new Dustbin(1200,650)
+	
+
+	//Create the Bodies Here.
+
+	 ball = new Paper(200,450,40)
+	 groundobj =new Ground(800,670,1600,20)
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  ball.display();
+  groundobj.display();
+  dustbinobj.display();
+
+ 
+}
+
+function keyPressed(){
+
+	
+	if(keyCode===UP_ARROW){
+
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y:-85})
+	}
+}
+
+
+
+
+
+
